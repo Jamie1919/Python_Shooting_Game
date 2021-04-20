8dd54ce34098fe3303c6fdebf05b4400d015b61e
Jamie1919 <jayprice882@gmail.com>
2021-04-20 13:55:34 +0200

    commit message

--- Game/10.1 Range7_Refinement.py
+++ Game/10.1 Range7_Refinement.py
@@ -1,23 +1,21 @@
 import pygame, sys, random
 
-
 pygame.init()
-
-screen = pygame.display.set_mode((1000,700))
+screen = pygame.display.set_mode((1280,720))
 clock = pygame.time.Clock()
 pygame.mouse.set_visible(False)
 
-wood_bg = pygame.image.load('Wood_BG.png')
-land_bg = pygame.image.load('Land_BG.png')
-water_bg = pygame.image.load('Water_BG.png')
-cloud1 = pygame.image.load('cloud1.png')
-cloud2 = pygame.image.load('cloud2.png')
+wood_bg = pygame.image.load('BG_Wood.png')
+land_bg = pygame.image.load('BG_Grass2.png')
+water_bg = pygame.image.load('Water2.png')
+cloud1 = pygame.image.load('Cloud1.png')
+cloud2 = pygame.image.load('Cloud2.png')
 crosshair = pygame.image.load('crosshair_blue_large.png')
-duck_surface = pygame.image.load('duck_target_white.png')
+duck_surface = pygame.image.load('duck_white.png')
 
 game_font = pygame.font.Font(None,60)
-text_surface = game_font.render('You Won!',True,(0,0,0))
-text_rect = text_surface.get_rect(center = (500,350))
+text_surface = game_font.render('You Won!',True,(255,255,255))
+text_rect = text_surface.get_rect(center = (640,360))
 
 land_position_y = 560
 land_speed = 1
@@ -27,8 +25,8 @@ water_speed = 1.5
 
 duck_list = []
 for duck in range(20):
-	duck_position_x = random.randrange(50,900)
-	duck_position_y = random.randrange(120,400)
+	duck_position_x = random.randrange(50,1200)
+	duck_position_y = random.randrange(120,600)
 	duck_rect = duck_surface.get_rect(center = (duck_position_x, duck_position_y))
 	duck_list.append(duck_rect)
 
@@ -39,12 +37,11 @@ while True:
 			sys.exit()
 		if event.type == pygame.MOUSEMOTION:
 			crosshair_rect = crosshair.get_rect(center = event.pos)
-		if event.type == pygame.MOUSEMOTION:
+		if event.type == pygame.MOUSEBUTTONDOWN:
 			for index,duck_rect in enumerate(duck_list):
 				if duck_rect.collidepoint(event.pos):
 					del duck_list[index]
 
-	
 	screen.blit(wood_bg,(0,0))
 
 	for duck_rect in duck_list:
@@ -55,26 +52,23 @@ while True:
 
 	land_position_y -= land_speed
 
-	if land_position_y <= 520 or land_position_y >=600:
+	if land_position_y <= 520 or land_position_y >= 600:
 		land_speed *= -1
 	screen.blit(land_bg,(0,land_position_y))
 
-	water_position_y -= water_speed
-
-	if water_position_y <= 620 or water_position_y >=680:
+	water_position_y += water_speed
+	if water_position_y <= 620 or water_position_y >= 680:
 		water_speed *= -1
 	screen.blit(water_bg,(0,water_position_y))
 
-	for duck_rect in duck_list:
-		screen.blit(duck_surface,duck_rect)
-
 	screen.blit(crosshair,crosshair_rect)
 	screen.blit(cloud1,(100,50))
-	screen.blit(cloud2,(100,160))
+	screen.blit(cloud2,(170,80))
 
-	screen.blit(cloud1,(600,50))
-	screen.blit(cloud2,(700,150))
+	screen.blit(cloud1,(604,10))
+	screen.blit(cloud2,(1000,90))
+	screen.blit(cloud1,(412,55))
 
+	
 	pygame.display.update()
 	clock.tick(120)
-
